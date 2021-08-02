1ab07d9d749b00c9afa182a8db41cd974549c11f
samanayubi@gmail.com <samanayubi@gmail.com>
2021-08-02 20:38:34 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LandingPageComponent } from '../app/components/landing-page/landing-page.component';
+import { CityComponent } from '../app/components/city/city.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    CityComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/city/city.component.ts
@@ -0,0 +1,12 @@
+
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'city-page',
+  templateUrl: './city.component.html',
+  styleUrls: ['./city.component.scss']
+})
+export class CityComponent {
+  title = 'weather-app';
+}
